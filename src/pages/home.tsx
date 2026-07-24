0e369ab7b3ab69641b955ba13ef85b4557e41f14
tgrif-dev <toby81260@gmail.com>
2026-07-24 10:00:28 +0100

    efwf

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,6 @@ import { lazy, Suspense } from 'react'
 import Hero from "../components/heroSection";
 import GameOfTheYearSection from "../components/GOTYSection";
 import CallToActionSecion from "../components/ctaSection";
-import VideoTrailerSection from "../components/trailerSection";
 import NewsletterSection from "../components/newsletterSection";
 import { Box, VStack } from '@chakra-ui/react'
 import { pageOffset } from '../styles/section'
